5585bf7a24ae422822b4fa5398bafab15b2fb591
sampathd11223344-sys <sampathd11223344@gmail.com>
2026-04-01 20:14:40 +0530

    Create Whatsapp.jsx

---
+++ client/src/components/Whatsapp.jsx
@@ -0,0 +1,20 @@
+export default function Whatsapp() {
+  return (
+    <a
+      href="https://wa.me/91891985233?text=Hello%20AB%20Clicks"
+      target="_blank"
+      rel="noreferrer"
+      style={{
+        position: "fixed",
+        bottom: "20px",
+        right: "20px",
+        background: "green",
+        color: "white",
+        padding: "10px 15px",
+        borderRadius: "50px"
+      }}
+    >
+      WhatsApp
+    </a>
+  );
+}
