4256c6f1ee2033f58df36eaf561d6cdf0662e1fc
sampathd11223344-sys <sampathd11223344@gmail.com>
2026-04-01 20:18:44 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,24 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const Booking = require("./models/Booking");
 
-app.get("/", (req, res) => {
-  res.send("AB Clicks Backend Running");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("YOUR_MONGO_URI")
+  .then(() => console.log("MongoDB Connected"));
+
+app.post("/api/book", async (req, res) => {
+  const booking = new Booking(req.body);
+  await booking.save();
+  res.send("Saved");
+});
+
+app.get("/api/bookings", async (req, res) => {
+  const data = await Booking.find();
+  res.json(data);
 });
 
 app.listen(5000, () => console.log("Server running"));
