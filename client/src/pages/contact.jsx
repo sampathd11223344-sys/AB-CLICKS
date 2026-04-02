d7b53153810cbeb2ffc64605f9f5867d969c5412
sampathd11223344-sys <sampathd11223344@gmail.com>
2026-04-02 12:08:01 +0530

    Create contact.jsx

---
+++ client/src/pages/contact.jsx
@@ -0,0 +1,18 @@
+export default function Contact() {
+  return (
+    <div style={{ background: "#000", color: "#fff", padding: "100px", textAlign: "center" }}>
+      <h1>Book Your Shoot</h1>
+
+      <a href="https://wa.me/91891985233" target="_blank">
+        <button style={{
+          marginTop: "20px",
+          padding: "12px 25px",
+          background: "#fff",
+          border: "none"
+        }}>
+          Book Now
+        </button>
+      </a>
+    </div>
+  );
+}
