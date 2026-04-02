b83b41ecd62db8eba60298ccf05138d8b1970c9b
sampathd11223344-sys <sampathd11223344@gmail.com>
2026-04-02 12:52:16 +0530

    Update Contact.jsx

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+
 export default function Contact() {
   return (
-    <div style={{ background: "#000", color: "#fff", padding: "100px", textAlign: "center" }}>
-      <h1>Book Your Shoot</h1>
+    <div style={{
+      background: "#000",
+      color: "#fff",
+      padding: "120px 20px",
+      textAlign: "center"
+    }}>
+      <h1>Let’s Create Together</h1>
 
       <a href="https://wa.me/91891985233" target="_blank">
         <button style={{
-          marginTop: "20px",
-          padding: "12px 25px",
+          marginTop: "30px",
+          padding: "14px 30px",
           background: "#fff",
-          border: "none"
+          border: "none",
+          cursor: "pointer"
         }}>
           Book Now
         </button>
