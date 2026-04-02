d243f7f322865d8637c16353c1cf8c6a6a940c48
sampathd11223344-sys <sampathd11223344@gmail.com>
2026-04-02 12:51:07 +0530

    Update Journey.jsx

--- client/src/pages/Journey.jsx
+++ client/src/pages/Journey.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+
 export default function Journey() {
   return (
-    <div style={{ padding: "100px 20px", background: "#000", color: "#fff" }}>
-      <h1>Our Journey</h1>
+    <div style={{
+      padding: "120px 20px",
+      background: "#000",
+      color: "#fff",
+      textAlign: "center"
+    }}>
+      <h1 style={{ marginBottom: "20px" }}>Our Journey</h1>
 
-      <p style={{ maxWidth: "600px", marginTop: "20px" }}>
+      <p style={{
+        maxWidth: "600px",
+        margin: "auto",
+        lineHeight: "1.8"
+      }}>
         We started with a passion for capturing emotions.
-        Today, we create cinematic wedding stories.
+        Today, we create cinematic wedding stories that last forever.
       </p>
     </div>
   );
