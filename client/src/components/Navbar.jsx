6655ea69b83ce07b91ffce3eddefb41b7bd56087
sampathd11223344-sys <sampathd11223344@gmail.com>
2026-04-02 12:03:28 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,9 +2,25 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div style={{ padding: "10px", background: "#111", color: "#fff" }}>
-      <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
-      <Link to="/booking">Book</Link>
+    <div style={{
+      position: "fixed",
+      top: 0,
+      width: "100%",
+      display: "flex",
+      justifyContent: "space-between",
+      padding: "20px",
+      background: "rgba(0,0,0,0.5)",
+      backdropFilter: "blur(10px)",
+      zIndex: 1000
+    }}>
+      <h3>AB Clicks</h3>
+
+      <div style={{ display: "flex", gap: "20px" }}>
+        <Link to="/">Home</Link>
+        <Link to="/journey">Journey</Link>
+        <Link to="/work">Work</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
     </div>
   );
 }
