0160aa87160ca8b73989be4a0167bb994d761b1b
sampathd11223344-sys <sampathd11223344@gmail.com>
2026-04-02 19:07:48 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Journey from "./pages/Journey";
 import Work from "./pages/Work";
 import Contact from "./pages/Contact";
-import Navbar from "./components/Navbar";
 
-function App() {
+export default function App(){
   return (
     <Router>
       <Navbar />
-
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/journey" element={<Journey />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Home/>}/>
+        <Route path="/journey" element={<Journey/>}/>
+        <Route path="/work" element={<Work/>}/>
+        <Route path="/contact" element={<Contact/>}/>
       </Routes>
     </Router>
   );
 }
-
-export default App;
